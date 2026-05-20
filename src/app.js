efd0a0b38558c9562362fc4e789b2269c4bfb89b
Vic Hagens <vic.hagens@student.vives.be>
2026-05-20 18:15:29 +0200

    swagger api-docs en het werkt!!!!

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
 // Import route handlers
 const brands = require("./routes/brands");
 const consoles = require("./routes/consoles");
@@ -12,11 +15,13 @@ const errorHandler = require("./middleware/error");
 
 const app = express();
 
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
+
 app.use(express.json());
 
 // greeting home route
 app.get("/", (req, res) => {
-  res.send("Welcome to the ConsoleCatalog API by VicH!");
+  res.send("Welcome to the ConsoleCatalog API of VicH!");
 });
 
 // API routes
@@ -27,6 +32,8 @@ app.use("/api/games", games);
 app.use("/api/reviews", reviews);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
